5d92efdf422b128e55d4184cd7b68c846caf1d72
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-07-11 23:28:55 +0300

    add test function

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,7 +2,20 @@ import ArticleList from "../../components/ArticleList";
 import Filter from "../../components/Filter";
 import { Container } from "../../styles/container.styled";
 
+function qwe(arr: Array<number>): any {
+  let result: any = {};
+  arr.forEach(function (a) {
+    if (a) {
+      result[a] = result[a] + 1 || 1;
+    }
+  });
+  const res = Object.values(result).sort();
+  return res[res.length - 1];
+}
+console.log(qwe([0, 1]));
 export default function HomePage() {
+  // const arr = [2, 2, 2];
+  // qwe(arr);
   return (
     <Container>
       <Filter />
